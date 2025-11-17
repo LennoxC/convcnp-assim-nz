87876dd96609c07d84c8e01aa39357c8c1844fb7
lennoxc <lennoxcrowe16@gmail.com>
2025-11-17 20:15:20 +0000

    env helper

--- src/config/env_loader.py
+++ src/config/env_loader.py
@@ -6,10 +6,18 @@
 # an example .env file is provided as .env_template
 #
 # environmental variables are then loaded with os.getenv("VAR_NAME")
-
+# you can also use the helper function get_env_var("VAR_NAME", default_value). If the variable is not found, it returns default_value.
+# e.g., DATA_HOME = get_env_var("DATA_HOME", "/default/path/to/data")
 
 from pathlib import Path
 from dotenv import load_dotenv
+import os
 
 ENV_PATH = Path(__file__).resolve().parents[2] / ".env"
 load_dotenv(ENV_PATH, override=False)
+
+def get_env_var(var_name, default=None):
+    """
+    Get an environment variable or return a default value.
+    """
+    return os.getenv(var_name, default)
